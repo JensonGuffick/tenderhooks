81810793ad5d01530fd9d5aa359bc1ead9774ab5
Jens <jensonguffick@yahoo.com>
2024-09-19 18:54:00 +0100

    4.3.6MI

--- scripts/countdown-1.js
+++ scripts/countdown-1.js
@@ -6,7 +6,7 @@ var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 if (distance < 0) {
     clearInterval(x);
     document.getElementById("countdown").innerHTML = "Album released!";
