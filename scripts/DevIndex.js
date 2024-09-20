43c7cef307d09cac4495c1ea7e9f2dcd4553cec3
Jens <jensonguffick@yahoo.com>
2024-09-20 16:50:24 +0100

    4.4.3MI

---
+++ scripts/DevIndex.js
@@ -0,0 +1,7 @@
+function DevIndex() {
+  if (confirm("Would you like to see the index?")) {
+    window.location.href =  "https://tenderhooks.uk/dev/devindex";
+  } else {
+    window.location.href = '#';
+  }
+}
