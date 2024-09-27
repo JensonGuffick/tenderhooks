8b91917a31c94514fb91142944dc6f76c6c82777
Jens <jensonguffick@yahoo.com>
2024-09-27 17:11:47 +0100

    4.5.2MI

--- scripts/offline.js
+++ scripts/offline.js
@@ -3,6 +3,7 @@
     const override = urlParams.get('override');
     if (override === 'yes') {
         console.log("Offline redirect is temporarily disabled by query parameter.");
+        return;
     }
     fetch('https://raw.githubusercontent.com/JensonGuffick/tenderhooks/main/offline.txt')
         .then(response => response.text())
